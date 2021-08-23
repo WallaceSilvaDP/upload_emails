825434255213d2a887ecbaa04761f181d504be25
wallacesilvadp <wallacedpaula97@gmail.com>
2021-08-23 16:38:01 -0300

    Melhorias

--- upload_emails/Controllers/UploadController.cs
+++ upload_emails/Controllers/UploadController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using upload_emails.Models;
-
+using upload_emails.Util;
 namespace upload_emails.Controllers
 {
     public class UploadController : Controller
@@ -19,17 +19,18 @@ namespace upload_emails.Controllers
         UploadModel oModelArquivos = new UploadModel();
         //Define uma instância de IHostingEnvironment
         IWebHostEnvironment _appEnvironment;
-
-        //Injeta a instância no construtor para poder usar os recursos
+        UploadUtil upload;
         public UploadController(IWebHostEnvironment env)
         {
             _appEnvironment = env;
+            upload = new UploadUtil(env);
+            
         }
         public IActionResult Index()
         {
             return View();
         }
-       
+
         //método para enviar os arquivos usando a interface IFormFile
         public async Task<IActionResult> UploadArquivo(IFormFile arquivo)
         {
@@ -43,84 +44,9 @@ namespace upload_emails.Controllers
             {
                 await arquivo.CopyToAsync(stream);
             }
-
-            separarArquivo(nomeArquivo);
-            return View(GetArquivos());
-        }
-
-        private Boolean ValidateEmail(string email_recebido)
-        {
-            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-            Match match = regex.Match(email_recebido);
-            if (match.Success)
-                return true;
-            else
-                return false;
-        }
-        private void separarArquivo(string nome_arquivo)
-        {
-            int counter = 0;
-            string line;
-
-            string caminhoDestinoArquivo = _appEnvironment.WebRootPath + @"\Arquivos";
-
-            StreamReader file = new StreamReader(caminhoDestinoArquivo + @"\Recebidos\" + nome_arquivo);
-            StreamWriter arquivoNovo = new StreamWriter(caminhoDestinoArquivo + @"\Novos\01.txt", false, Encoding.ASCII);
-
-            ArrayList emailsNovo = new ArrayList();
             
-            while ((line = file.ReadLine()) != null)
-            {
-                if (ValidateEmail(line) && !emailsNovo.Contains(line)) {
-                    emailsNovo.Add(line + Environment.NewLine);
-                    arquivoNovo.Write(line + Environment.NewLine);
-                    counter++;
-                }
-
-                if (counter % 5 == 0)
-                {
-                    arquivoNovo.Close();
-                    arquivoNovo = new StreamWriter(caminhoDestinoArquivo + @"\Novos\0" + ((counter / 5) + 1).ToString() + ".txt", false, Encoding.ASCII);
-                    emailsNovo = new ArrayList();
-                }
-                
-            }
-            arquivoNovo.Close();
-            System.IO.File.Delete(caminhoDestinoArquivo + @"\Novos\0" + ((counter / 5) + 1).ToString() + ".txt");
-
-            file.Close();
-        }
-        public List<UploadModel> GetArquivos()
-        {
-
-            List<UploadModel> lstArquivos = new List<UploadModel>();
-            string caminhoDestinoArquivo = _appEnvironment.WebRootPath + @"\Arquivos\";
-            string caminhoDestinoArquivoOriginal = caminhoDestinoArquivo + @"\Novos\";
-            DirectoryInfo dirInfo = new DirectoryInfo(caminhoDestinoArquivoOriginal);
-            
-            int i = 0;
-            foreach (var item in dirInfo.GetFiles())
-            {
-
-                StreamReader file = new StreamReader(dirInfo.FullName + item.Name);
-                string line;
-                List<string> emails = new List<string>();
-                while ((line = file.ReadLine()) != null)
-                {
-                    emails.Add(line);
-                }
-
-                file.Close();
-                lstArquivos.Add(new UploadModel()
-                {
-                    arquivoID = i + 1,
-                    arquivoNome = item.Name,
-                    arquivoCaminho = dirInfo.FullName + item.Name,
-                    emails = emails
-                });
-                i = i + 1;
-            }
-            return lstArquivos;
+            upload.separarArquivo(nomeArquivo);
+            return View(upload.GetArquivos());
         }
 
         public async Task<FileResult> Download([FromQuery] string file)
@@ -137,18 +63,9 @@ namespace upload_emails.Controllers
             }
             memory.Position = 0;
 
-            return File(memory, GetContentType(filePath), file);
+            return File(memory, upload.GetContentType(filePath), file);
         }
-        private string GetContentType(string path)
-        {
-            var provider = new FileExtensionContentTypeProvider();
-            string contentType;
-            if (!provider.TryGetContentType(path, out contentType))
-            {
-                contentType = "application/octet-stream";
-            }
-            return contentType;
-        }
+
     }
 }
 
---
+++ upload_emails/Util/UploadUtil.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using upload_emails.Models;
+
+namespace upload_emails.Util
+{
+    public class UploadUtil
+    {
+        IWebHostEnvironment _appEnvironment;
+        public UploadUtil(IWebHostEnvironment env)
+        {
+            _appEnvironment = env;
+        }
+
+
+        private Boolean ValidateEmail(string email_recebido)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email_recebido);
+            if (match.Success)
+                return true;
+            else
+                return false;
+        }
+        public void separarArquivo(string nome_arquivo)
+        {
+            int counter = 0;
+            string line;
+
+            string caminhoDestinoArquivo = _appEnvironment.WebRootPath + @"\Arquivos";
+
+            StreamReader file = new StreamReader(caminhoDestinoArquivo + @"\Recebidos\" + nome_arquivo);
+            StreamWriter arquivoNovo = new StreamWriter(caminhoDestinoArquivo + @"\Novos\01.txt", false, Encoding.ASCII);
+
+            ArrayList emailsNovo = new ArrayList();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (ValidateEmail(line) && !emailsNovo.Contains(line))
+                {
+                    emailsNovo.Add(line + Environment.NewLine);
+                    arquivoNovo.Write(line + Environment.NewLine);
+                    counter++;
+                }
+
+                if (counter % 5 == 0)
+                {
+                    arquivoNovo.Close();
+                    arquivoNovo = new StreamWriter(caminhoDestinoArquivo + @"\Novos\0" + ((counter / 5) + 1).ToString() + ".txt", false, Encoding.ASCII);
+                    emailsNovo = new ArrayList();
+                }
+
+            }
+            arquivoNovo.Close();
+            System.IO.File.Delete(caminhoDestinoArquivo + @"\Novos\0" + ((counter / 5) + 1).ToString() + ".txt");
+
+            file.Close();
+        }
+        public List<UploadModel> GetArquivos()
+        {
+
+            List<UploadModel> lstArquivos = new List<UploadModel>();
+            string caminhoDestinoArquivo = _appEnvironment.WebRootPath + @"\Arquivos\";
+            string caminhoDestinoArquivoOriginal = caminhoDestinoArquivo + @"\Novos\";
+            DirectoryInfo dirInfo = new DirectoryInfo(caminhoDestinoArquivoOriginal);
+
+            int i = 0;
+            foreach (var item in dirInfo.GetFiles())
+            {
+
+                StreamReader file = new StreamReader(dirInfo.FullName + item.Name);
+                string line;
+                List<string> emails = new List<string>();
+                while ((line = file.ReadLine()) != null)
+                {
+                    emails.Add(line);
+                }
+
+                file.Close();
+                lstArquivos.Add(new UploadModel()
+                {
+                    arquivoID = i + 1,
+                    arquivoNome = item.Name,
+                    arquivoCaminho = dirInfo.FullName + item.Name,
+                    emails = emails
+                });
+                i = i + 1;
+            }
+            return lstArquivos;
+        }
+
+        
+        public string GetContentType(string path)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+            string contentType;
+            if (!provider.TryGetContentType(path, out contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return contentType;
+        }
+    }
+}
+
